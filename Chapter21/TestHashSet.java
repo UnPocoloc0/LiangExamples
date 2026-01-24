2707b61171ccb1f6200f81c0b05f94a8266bf944
UnPocoloc0 <116169637+UnPocoloc0@users.noreply.github.com>
2026-01-24 17:43:36 +0200

    Update TestHashSet.java

--- Chapter21/TestHashSet.java
+++ Chapter21/TestHashSet.java
@@ -1,19 +1,36 @@
 import java.util.*;
-public class TestHashSet {
-        public static void main(String[] args) {
-                // Create a hash set
-                Set<String> set = new HashSet<>();
-                // Add strings to the set
-                set.add("London");                set.add("Paris");                set.add("New York");                set.add("San Francisco");                set.add("Beijing");
-                // Duplikaatti
+
+public class TestHashSet {
+    
+    public static void main(String[] args) {
+        
+        // Create a hash set
+        
+        Set<String> set = new HashSet<>();
+        
+        // Add strings to the set
+        
+        set.add("London");        
+        set.add("Paris");        
+        set.add("New York");        
+        set.add("San Francisco");        
+        set.add("Beijing");
+        
+        // Duplikaatti
         set.add("New York");
-                // Duplikaatti ei näy tulostuksessa, alkiot tulostuvat satunnaisessa järjestyksessä 
+        
+        // Duplikaatti ei näy tulostuksessa, alkiot tulostuvat satunnaisessa järjestyksessä 
         System.out.println(set);
-                // Display the elements in the hash set
-                for (String s: set) { 
+        
+        // Display the elements in the hash set
+        
+        for (String s: set) { 
         // Samalle riville tulostus, erottimena välilyönti 
             System.out.print(s.toUpperCase() + " ");
-                    }     } }
+            
+        } 
+    } 
+}
 /*
 1️⃣ YLÄTASON SUUNNITELMA
 
