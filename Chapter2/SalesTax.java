0e3cf19d8dfd13c8defb7c5795206517212cba04
Simo Loikkanen <loikkanensimo@gmail.com>
2026-06-02 20:04:22 +0300

    Chapter2 examples

---
+++ Chapter2/SalesTax.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SalesTax {
+		public static void main(String[] args) {
+		// Input Stream -> keyboard
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.print("Enter purchase amount: ");
+		double purchaseAmount = input.nextDouble(); 
+		
+		double tax = purchaseAmount * 0.06;
+		// Truncate not round -> old way
+		System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);
+	} 
+}
